632dc9b34a07b01cb093fd0e7b3320da34386361
pierinacamarena <pcamaren@student.42.fr>
2023-05-03 20:50:20 +0200

    MessageInput: `handleSubmit` send message on enter

--- frontend/src/component/Chat/MessageInput.tsx
+++ frontend/src/component/Chat/MessageInput.tsx
@@ -3,14 +3,22 @@ import React, {useState} from "react";
 export default function MessageInput({send} : {send: (val: string) => void})
 {
     const [value, setValue] = useState("")
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        send(value);
+        setValue("");
+    };
+
     return (
-        <>
+        <form onSubmit={handleSubmit}>
             <input
             onChange={(e)=>setValue(e.target.value)}
             placeholder="Type your message"
             value={value}
             />
-            <button onClick={() => send(value)}>Send</button>
-        </>
-    )
+            <button type="submit">Send</button>
+            {/* <button onClick={() => send(value)}>Send</button> */}
+        </form>
+    );
 }
