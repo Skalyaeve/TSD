00cfca8d19fda13c21e8ca83f47797668ebc779f
Julien CHÊNE <jchene@student.42.fr>
2023-05-12 16:44:31 +0200

    updated webpack config

--- test-scratch/webpack.config.js
+++ test-scratch/webpack.config.js
@@ -1,21 +1,66 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-	entry: './server/authoritative_server/js/game.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'game.bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
+module.exports = [
+	{
+		entry: './server/authoritative_server/js/game.js',
+		output: {
+			path: path.resolve(__dirname, 'server/authoritative_server/dist'),
+			filename: 'game.headless.bundle.js'
+		},
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: [
+						/server\/authoritative_server/
+					],
+					use: {
+						loader: 'babel-loader'
+					}
+				},
+				{
+					test: /\.(png)$/i,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'dist/img',
+						},
+					},],
 				}
-			}
-		]
-	}
-};
+			]
+		}
+	},
+	{
+		entry: './server/public/js/game.js',
+		output: {
+			path: path.resolve(__dirname, 'server/public/dist'),
+			filename: 'game.client.bundle.js'
+		},
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: [
+						/server\/public/
+					],
+					use: {
+						loader: 'babel-loader'
+					}
+				},
+				{
+					test: /\.(png)$/i,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'dist/img',
+						},
+					},],
+				}
+			]
+		}
+	},
+]
