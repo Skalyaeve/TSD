98498d55dd3a786c6d3a5b656b4e15da62c22972
Julien CHÊNE <jchene@student.42.fr>
2023-05-17 17:04:25 +0200

    game service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatGateway } from './chat/chat.gateway';
 import { UserModule } from './user/user.module';
+import { GameService } from './game/game.service';
 
 @Module({
   imports: [
@@ -10,6 +11,6 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     UserModule,
   ],
-  providers: [ChatGateway],
+  providers: [ChatGateway, GameService],
 })
 export class AppModule {}
