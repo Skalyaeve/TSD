91726736cd111f3c4dd82f719e4f7ce7933d41f6
Julien CHÊNE <jchene@student.42.fr>
2023-05-18 19:37:48 +0200

    test game

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { ChatGateway } from './chat/chat.gateway';
 import { UserModule } from './user/user.module';
+import { ChatGateway } from './chat/chat.gateway';
 import { ChatService } from './chat/chat.service';
-import { GameService } from './game/game.service';
+import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     AuthModule,
     PrismaModule,
     UserModule,
+    GameModule,
   ],
-  providers: [ChatGateway, ChatService, GameService],
+  providers: [ChatGateway, ChatService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
