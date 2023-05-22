b1cbecc3eea2678758b43fa5f7dece25da40e2f6
pierinacamarena <pcamaren@student.42.fr>
2023-05-22 17:24:19 +0200

    🔥 removed providers from app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { UserModule } from './user/user.module.js';
-import { ChatGateway } from './chat/chat.gateway.js';
-import { ChatService } from './chat/chat.service.js';
 import { GameModule } from './game/game.module.js';
 import { ChatModule } from './chat/chat.module.js';
-import { AuthService } from './auth/auth.service.js';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { PrismaService } from 'nestjs-prisma';
 
 @Module({
   imports: [
@@ -22,12 +18,6 @@ import { PrismaService } from 'nestjs-prisma';
     ChatModule,
     ConfigModule,
 	GameModule
-  ],
-  providers: [
-    ChatService, 
-    AuthService,
-    ConfigService,
-    PrismaService
-  ],
+  ]
 })
 export class AppModule {}
