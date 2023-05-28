67219122458d2b63ab21fc005c33f0aee805ae24
pierinacamarena <pcamaren@student.42.fr>
2023-05-28 18:06:57 +0200

    🚧 : channel events and database call methods

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -43,6 +43,17 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
     client.broadcast.emit('message', data); // Use broadcast.emit() to send the message to all clients except the sender
   }
 
+  //*****************************************************************************************************************************************//
+  //*****************************************************************************************************************************************//
+  //                                                          CHANNEL EVENTS                                                                 //
+  //*****************************************************************************************************************************************//
+  //*****************************************************************************************************************************************//
+
+
+  //--------------------------------------------------------------------------//
+  //                            CHANNEL SETUP EVENTS                          //
+  //--------------------------------------------------------------------------//
+
   /**
    * 'createChannel' event that creates a channel on the db
    * and emits to the client an event 'channelCreated' with the created channel
@@ -53,30 +64,23 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
   @SubscribeMessage('createChannel')
   async onCreateChannel(
     @ConnectedSocket() client: Socket, 
-    @MessageBody() data: {name: string; type: string; password?: string}) : Promise<void>
+    @MessageBody() data: {name: string; userId: number}) : Promise<void>
   {
-    const user = await this.chatService.getUserFromSocket(client);
-    const { name, type, password } = data;
+    const { name, userId } = data;
 
-    if (!user){
-      throw new WsException('Invalid credentials');
-    }
+    const channel = await this.chatService.createOneChannel(name, userId);
+    const chanID = channel.id;
+    await this.chatService.createOneChanMember(chanID, userId);
+    await this.chatService.makeOwnerAdmin(userId, chanID);
 
-    if (!Object.values(ChanType).includes(type as ChanType)){
-      throw new WsException('Invalid channel type');
-    }
-
-    const channelType: ChanType = type as ChanType;
-
-    const channel = await this.chatService.createChannel({
-      name, 
-      type: channelType, 
-      passwd: password,
-      chanOwnerRef: {connect: {id: user.id}}
-    });
-
-    client.emit('channelCreated', channel);
+    this.server.emit('created channel');
+    // client.emit('channelCreated', channel);
   }
+
+
+  //--------------------------------------------------------------------------//
+  //                         CHANNEL RETRIEVE EVENTS                          //
+  //--------------------------------------------------------------------------//
 
   /**
    * Event to find a channel by id and emit it to the client
@@ -91,17 +95,54 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
   {
     try{
       const channel = await this.chatService.findChannelbyId(data.chanId);
-  
       if (!channel){
         throw new WsException('Channel not found');
       }
-  
       client.emit('channelFound', channel);
     }
     catch (error){
       console.log(error);
     }
   }
+
+  //--------------------------------------------------------------------------//
+  //                           CHANNEL MESSAGE EVENTS                         //
+  //--------------------------------------------------------------------------//
+
+  //to test the difference between the client.emit and the server.emit 
+  /**
+   * sendChanMessageEvent will create a message for the required channel
+   * and emit a 'sentChanMessage' back to the client with the chanMessage as data
+   * @param client 
+   * @param data 
+   */
+  @SubscribeMessage('sendChanMessage')
+  async handleSendChanMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: {senderId: number; chanId: number; content: string})
+  {
+    try {
+      const {senderId, chanId, content} = data;
+      const chanMessage = await this.chatService.createOneChanMessage(senderId, chanId, content);
+      if (!chanMessage) {
+        throw new WsException('Channel message was not sent');
+      }
+      const ChanRoomId = 'chan_'+ chanId + '_room';
+      client.to(ChanRoomId).emit('SentChanMessage', chanMessage);
+      // client.emit('SentChanMessage', chanMessage);
+
+      // this.server.in(ChanRoomId).emit('SentChanMessage', chanMessage);
+      // Remember that client.to(room) targets all sockets in a room, but not the sender. If you also want to include the sender, you could use:
+    }
+    catch (error)
+    {
+      console.log(error);
+    }
+  }
+
+  //--------------------------------------------------------------------------//
+  //                           CHANNEL MEMBER EVENTS                          //
+  //--------------------------------------------------------------------------//
 
   /**
    * JoinChannel event, will make the giver user join the givne channel
@@ -117,13 +158,23 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
     try {
       const {chanID, userID} = data;
       const chanMember = await this.chatService.createOneChanMember(chanID, userID);
-      
-      if (!chanMember)
-      {
+      if (!chanMember) {
         throw new WsException('Chan member did not joined');
       }
-      
-      client.emit('userJoinedChannel', chanMember);
+      else {
+        const userSockets = this.userSocketsService.getUserSocketIds(userID);
+        userSockets.forEach(SocketID =>{
+          const socket = this.server.sockets.sockets.get(SocketID);
+          if (socket) {
+            const ChanRoomId = 'chan_'+ chanID + '_room';
+            socket.join(ChanRoomId);
+          }
+        })
+        const userRoomId = 'userID_' + userID.toString() + '_room';
+        this.server.to(userRoomId).emit('joinRoom', String(chanID));
+        // client.emit('userJoinedChannel', chanMember);
+        // In the above code, this.server.to(userRoomId).emit('joinRoom', String(chanID)); will emit a 'joinRoom' event to all sockets in the user-specific room. You will then need to handle this 'joinRoom' event on the client-side, where each socket will join the channel room upon receiving the 'joinRoom' event.
+      }
     }
     catch (error){
       console.log(error);
@@ -131,31 +182,165 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
   }
 
   /**
-   * sendChanMessageEvent will create a message for the required channel
-   * and emit a 'sentChanMessage' back to the client with the chanMessage as data
+   * makeMemberAdmin event makes a given member admin
+   * and returns to the client an event with the updated
+   * channel member
    * @param client 
    * @param data 
    */
-  @SubscribeMessage('sendChanMessage')
-  async handleSendChanMessage(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() data: {senderId: number; chanId: number; content: string})
+  @SubscribeMessage('makeMemberAdmin')
+  async handleMakeMemberAdmin(
+      @ConnectedSocket() client: Socket,
+      @MessageBody() data: {chanOwnerId:number, chanId: number, memberId: number})
   {
     try {
-      const {senderId, chanId, content} = data;
-      const chanMessage = await this.chatService.createOneChanMessage(senderId, chanId, content);
-  
-      if (!chanMessage)
-      {
-        throw new WsException('Channel message was not sent');
+    const {chanId, memberId, chanOwnerId} = data;
+      const updatedChanMember = await this.chatService.makeChanAdmin(data);
+      if (!updatedChanMember) {
+        throw new WsException('could not make member admin');
       }
-      client.emit('SentChanMessage', chanMessage);
+      const ChanRoomId = 'chan_'+ chanId + '_room';
+      this.server.to(ChanRoomId).emit('newAdminInRoom');
+      // client.emit('MemberisAdmin', updatedChanMember);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  /**
+   * muteMember event, will emit to the client an event with the mutedMember
+   * @param client 
+   * @param data 
+   */
+  @SubscribeMessage('muteMember')
+  async handleMuteMember(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: {chanId: number, memberToMuteId: number, adminId: number, muteDuration: number})
+  {
+    try {
+      const {chanId, memberToMuteId, adminId, muteDuration} = data;
+      const mutedMember = await this.chatService.muteMember(data);
+      if (!mutedMember) {
+        throw new WsException('could not mute member');
+      }
+      else {
+        const ChanRoomId = 'chan_'+ chanId + '_room';
+        this.server.to(ChanRoomId).emit('memberMuted');
+        // client.emit('memberIsMuted', mutedMember);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  @SubscribeMessage('banMember')
+  async handleBanMember(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: {chanId: number, memberToBanId: number, adminId: number})
+  {
+    try {
+      const {chanId, memberToBanId, adminId} = data;
+      const bannedMember = await this.chatService.banMember(data);
+      if (!bannedMember)  {
+        throw new WsException('could not ban member')
+      }
+      else {
+        
+        const ChanRoomId = 'chan_'+ chanId + '_room';   
+        this.server.to(ChanRoomId).emit('memberBanned');
+        // client.emit('memberIsBanned', bannedMember);
+      }
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  @SubscribeMessage('kickMember')
+  async handleKickMember(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: {chanId: number, memberToKickId: number, adminId: number})
+  {
+    try {
+      const {chanId, memberToKickId, adminId} = data;
+      const kickedMember = await this.chatService.kickMember(data);
+      if (!kickedMember)  {
+        throw new WsException('could not ban member')
+      }
+      else {
+        const ChanRoomId = 'chan_'+ chanId + '_room';   
+        this.server.to(ChanRoomId).emit('memberKicked');
+        // client.emit('memberIsBanned', kickedMember);
+      }
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  //--------------------------------------------------------------------------//
+  //                           CHANNEL UPDATE EVENTS                          //
+  //--------------------------------------------------------------------------//
+
+  /**
+   * event setChannelName to change the name of a channel
+   * will check the user that is changing the name is admin
+   * emit back to the client an event with the updated channel
+   * @param client 
+   * @param data 
+   */
+  @SubscribeMessage('setChannelName')
+  async handleSetChannelName(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: {chanMember:number, chanId:number, newChanName:string})
+  {
+    try {
+      const {chanMember, chanId, newChanName} = data;
+      const updatedChannel = await this.chatService.setChanName(data);
+      if (!updatedChannel) {
+        throw new WsException('could not change name of channel');
+      }
+      const ChanRoomId = 'chan_'+ chanId + '_room';
+      const chanName = updatedChannel.name;
+      this.server.to(ChanRoomId).emit('chanNameChanged', chanName);
+      // client.emit('channelNameChanged', updatedChannel);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * setNewPasswd event to update the password of a channel
+   * returns the client an event with the updated channel
+   * @param client 
+   * @param data 
+   */
+  @SubscribeMessage('setNewPasswd')
+  async handleSetNewUserPassword(
+      @ConnectedSocket() client: Socket,
+      @MessageBody() data: {chanId: number; userId: number; newPasswd: string})
+  {
+    try {
+      const {chanId, userId, newPasswd} = data;
+      const updatedChannel = await this.chatService.setChanPassword(data);
+
+      if (!updatedChannel)
+      {
+        throw new WsException('Password could not be set');
+      }
+      client.emit('chanPasswdChanged', updatedChannel);
     }
     catch (error)
     {
       console.log(error);
     }
   }
+
+  //--------------------------------------------------------------------------//
+  //                        CONNECTION SET UP EVENTS                          //
+  //--------------------------------------------------------------------------//
 
   /**
    * 'getUserInfo: event to get the user information of current client
@@ -185,7 +370,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
       console.log(error);
     }
   }
-
+  
   /**
    * Still don't know the purpose of this :|
    * @param server 
@@ -218,6 +403,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
       const userWithSocket = this.userSocketsService.getUserSocketIds(userID);
       console.log('userWithSocket: ',userWithSocket);
       const userRoomId = 'userID_' + userID.toString() + '_room';
+      const userRooms = this.chatService.findAllChannelsByMember(userID);
+
+      
       client.join(userRoomId);
 
       // const userChannels = 
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -35,8 +35,18 @@ export class ChatService {
     //Creation methods, equivalent of setters
 
     async createChannel(channelData: { name: string, type: ChanType, passwd?: string, chanOwnerRef: { connect: { id: number } } }) :Promise<Channel>{
-        const channel = await this.prisma.channel.create({
+        const channel: Channel = await this.prisma.channel.create({
           data: channelData,
+        });
+        return channel;
+    }
+
+    async createOneChannel(name:string, chanOwner: number): Promise<Channel> {
+        const channel = this.prisma.channel.create({
+            data: {
+                name,
+                chanOwner,
+            },
         });
         return channel;
     }
@@ -72,13 +82,220 @@ export class ChatService {
 
     //setters that don't create but change values
 
-    async setChanPassword(chanId: number, userId: number, content: string)
-    {
-        if (this.isAdmin(chanId, userId))
-        {
-            
+    async setChanPassword(data: {chanId: number, userId: number, newPasswd: string }) {
+        try {
+            const {chanId, userId, newPasswd} = data;
+
+            if (this.isAdmin(chanId, userId))
+            {
+                const updatedChannel = await this.prisma.channel.update({
+                        where: {id: chanId},
+                        data: {passwd: newPasswd}
+                    }
+                );
+                if (!updatedChannel) {
+                    throw new Error("Error in setting new passwd");
+                }
+                return updatedChannel;
+            }
+            else {
+                throw new Error ("User is not admin");
+            }
+        }
+        catch (error) {
+            console.log(error);
+            throw error;
         }
     }
+
+    async setChanName(data: {chanMember:number; chanId: number, newChanName: string}){
+        try {
+            const {chanMember, chanId, newChanName} = data;
+            const isAdmin = await this.isAdmin(chanId, chanMember);
+            if (!isAdmin){
+                throw new Error('Member does not have channel privileges')
+            }
+            else {
+                const updatedChannel = await this.prisma.channel.update(
+                    {
+                        where: {id: chanId},
+                        data: {name: newChanName}
+                    }
+                );
+                if (!updatedChannel) {
+                    throw new Error("Could not change name");
+                }
+                return updatedChannel;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async makeOwnerAdmin( userId: number, chanId: number)
+    {
+        try {
+            const updatedMember: ChanMember|null = await this.prisma.chanMember.update({
+                where: {
+                    chanId_member: {
+                        chanId,
+                        member: userId,
+                    },
+                },
+                data: {
+                    isAdmin: true,
+                }
+            });
+            if (!updatedMember) {
+                throw new Error('could not make user admin of channel');
+            }
+            return updatedMember;
+        }
+        catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async makeChanAdmin(data: {chanOwnerId:number, chanId: number, memberId: number}) {
+        try {
+            const {chanOwnerId, chanId, memberId} = data;
+
+            const isOwner = await this.isOwner(chanId, chanOwnerId);
+            if (!isOwner) {
+                throw new WsException('not channel owner');
+            }
+            else {
+                const updatedMember: ChanMember|null = await this.prisma.chanMember.update({
+                    where: {
+                        chanId_member: {
+                            chanId,
+                            member: memberId,
+                        },
+                    },
+                    data: {
+                        isAdmin: true,
+                    }
+                });
+    
+                if (!updatedMember) {
+                    throw new Error('could not make user admin of channel');
+                }
+                return updatedMember;
+            }
+        }
+        catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async muteMember(data: {chanId: number, memberToMuteId: number, adminId: number, muteDuration: number}) {
+
+        try {
+            const {chanId, memberToMuteId, adminId, muteDuration} = data;
+            const checkAdmin = await this.isAdmin(chanId, adminId);
+            if (!checkAdmin) {
+                throw new Error('user does not have privileges');
+            }
+            else {
+                const isOwner = await this.isOwner(chanId, memberToMuteId);
+                if (isOwner) {
+                    throw new Error('Cannot mute a channel owner');
+                }
+                else {
+                    const muteEnd = new Date();
+                    // muteEnd.setSeconds(muteEnd.getSeconds() + muteDuration);
+                    muteEnd.setMinutes(muteEnd.getMinutes() + muteDuration);
+    
+                    const mutedMember = await this.prisma.chanMember.update({
+                        where: { chanId_member: {chanId, member: memberToMuteId}},
+                        data : {muteTime: muteEnd},
+                    });
+                    if (!mutedMember) {
+                        throw new Error('could not mute member');
+                    }
+                    return mutedMember;
+                }
+            }
+        }
+        catch (error)
+        {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async kickMember(data: {chanId: number, memberToKickId: number, adminId: number}) {
+
+        try {
+            const {chanId, memberToKickId, adminId} = data;
+            const checkAdmin = await this.isAdmin(chanId, adminId);
+            if (!checkAdmin) {
+                throw new Error('user does not have privileges');
+            }
+            else {
+                const isOwner = await this.isOwner(chanId, memberToKickId);
+                if (isOwner) {
+                    throw new Error('Cannot kick a channel owner');
+                }
+                else {
+                    const kickedMember = await this.prisma.chanMember.delete({
+                        where: {chanId_member: {chanId, member: memberToKickId}}
+                    });
+                    if (!kickedMember) {
+                        throw new Error('could not kick member');
+                    }
+                    return kickedMember;
+                }
+            }
+        }
+        catch (error)
+        {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async banMember(data: {chanId: number, memberToBanId: number, adminId: number}) {
+
+        try {
+            const {chanId, memberToBanId, adminId} = data;
+            const checkAdmin = await this.isAdmin(chanId, adminId);
+            if (!checkAdmin) {
+                throw new Error('user does not have privileges');
+            }
+            else {
+                const isOwner = await this.isOwner(chanId, memberToBanId);
+                if (isOwner) {
+                    throw new Error('Cannot mute a channel owner');
+                }
+                else {
+                    const bannedMember = await this.prisma.chanMember.delete({
+                        where: {chanId_member: {chanId, member: memberToBanId}}
+                    });
+
+                    await this.prisma.chanBan.create({
+                        data: {
+                            chanId,
+                            bannedUser: memberToBanId,
+
+                        }
+                    })
+                    return bannedMember;
+                }
+            }
+        }
+        catch (error)
+        {
+            console.log(error);
+            throw error;
+        }
+
+    }
+
     //Find methods, equivalent of getters
 
     async findChannelbyId(id: number): Promise<Channel|null> {
