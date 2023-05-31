465ae2818d95563fed8ec2096163bd5c29fde5c5
Julien CHÊNE <jchene@student.42.fr>
2023-05-31 18:25:11 +0200

    fixed bcrypt bug

--- frontend/src/tsx-components/Root.tsx
+++ frontend/src/tsx-components/Root.tsx
@@ -54,25 +54,7 @@ const LoginBtn: React.FC<LogginBtnProps> = ({ setLogged }) => {
 	const animating = useRef(false)
 
 	// ----HANDLERS--------------------------- //
-	const connect = async () => {
-		let address
-		let clientID
-		let redirectURI
-		if (process.env.OA42_API_ADDR)
-			address = process.env.OA42_API_ADDR
-		else return
-		if (process.env.OA42_API_KEY)
-			clientID = encodeURIComponent(process.env.OA42_API_KEY)
-		else return
-		if (process.env.OA42_API_REDIR)
-			redirectURI = encodeURIComponent(process.env.OA42_API_REDIR)
-		else return
-
-		const urlBase = `${address}?response_type=code`
-		const urlArg1 = `&redirect_uri=${redirectURI}`
-		const urlArg2 = `&client_id=${clientID}`
-		window.location.href = urlBase + urlArg1 + urlArg2
-
+	const connect = () => {
 		const servID = 'http://localhost:3000'
 		const path = '/auth/42/login'
 		try {
