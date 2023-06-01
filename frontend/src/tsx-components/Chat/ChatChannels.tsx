1be575f951696fd9c504f41ba85188cc0ff4153d
pierinacamarena <pcamaren@student.42.fr>
2023-06-01 23:59:30 +0200

    💄 channel create form modified

--- frontend/src/tsx-components/Chat/ChatChannels.tsx
+++ frontend/src/tsx-components/Chat/ChatChannels.tsx
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from "react";
 import { BsSearchHeart } from "react-icons/bs";
 import { BsPlusCircle } from "react-icons/bs";
 import Modal from 'react-modal';
+import "../../css/Chat/ChannelCreate.css"
 
 export default function ChatChannels()
-{
-    // useEffect(() => {
-    //     setIsModalOpen(true);
-    //   }, []);
-    
+{   
     const [channel, setChannel] = useState("");
     const [newChannel, setNewChannel] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -73,29 +70,44 @@ export default function ChatChannels()
         <Modal 
             isOpen={isModalOpen}
             onRequestClose={() => setIsModalOpen(false)}
+            className="modal-chan-create"
+            overlayClassName="overlay-chan-create"
         >
-            <h2>Create a new channel</h2>
-            <form onSubmit={handleSubmitNewChannel}>
-                <input 
-                    onChange={(e)=>setNewChannel(e.target.value)}
-                    placeholder="Type new channel name"
-                    value={newChannel}
-                />
-                <input 
-                    onChange={(e)=>setMembers(e.target.value)}
-                    placeholder="Type members"
-                    value={members}
-                />
-                <select 
-                    onChange={(e)=>setChannelType(e.target.value)}
-                    value={channelType}
-                >
-                    <option value="">Select channel type</option>
-                    <option value="private">Private</option>
-                    <option value="public">Public</option>
-                </select>
-                <button type="submit">Create Channel</button>
-            </form>
+            <div className="chan-create-grid">
+                <div className="create-title">
+                    <h2>Create a new channel</h2>
+                </div>
+                <form className="create-form" onSubmit={handleSubmitNewChannel}>
+                    <div className="create-name">
+                        <input 
+                            onChange={(e)=>setNewChannel(e.target.value)}
+                            placeholder="New channel name..."
+                            value={newChannel}
+                        />
+                    </div>
+                    <div className="select-members">
+                        <input 
+                            onChange={(e)=>setMembers(e.target.value)}
+                            placeholder="Add members"
+                            value={members}
+                        />
+                    </div>
+                    <div className="select-type">
+                        <select 
+                            onChange={(e)=>setChannelType(e.target.value)}
+                            value={channelType}
+                        >
+                            {/* <option value="" selected disabled>Select channel type</option> */}
+                            <option value="private">Private</option>
+                            <option value="public">Public</option>
+                            <option value="protected">Protected</option>
+                        </select>
+                    </div>
+                </form>
+                <div className="chan-create-submit">
+                    <button className="chan-create-btn" type="submit">Create Channel</button>
+                </div>
+            </div>
         </Modal>
     </div>
     )
